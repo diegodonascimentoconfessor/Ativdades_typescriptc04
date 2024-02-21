caf7004d7951fc66cb75cc51b32ac0adc2e953ae
diegodonascimentoconfessor <153656081+diegodonascimentoconfessor@users.noreply.github.com>
2024-02-21 17:22:32 -0300

    extends

--- senac/src/ts/exercicio.ts
+++ senac/src/ts/exercicio.ts
@@ -15,7 +15,18 @@ class Pessoa {
         this.data_nascimento = data_nascimento ;
          this.rg = rg 
     }
+    mostrarDados(){
+        console.log('Dados Pessoas')
+        console.log(`endereço:${this.endereco}`)
+        console.log(`nome:${this.nome}`)
+        console.log(`email:${this.email}`)
+        console.log(`telefone:${this.telefone}`)
+        console.log(`data de Nascimento:${this.data_nascimento.toLocaleString('pt-BR')}`)
+        console.log(`cpf:${this.rg}`)
+    }
 }
+
+
 
  // atributos Proprios da Classe Funcionario 
 class Funcionario extends Pessoa {
@@ -25,16 +36,19 @@ class Funcionario extends Pessoa {
     constructor(cargo: string, matricula: string, endereco:string[], nome:string,telefone:string, email:string,data_nascimento:Date, rg:string){
          super(endereco,nome, email, telefone, data_nascimento, rg)
         this.cargo =cargo;
-         this.matricula = matricula
+         this.matricula = matricula;
     }     
+    mostrarDados(){
+       super.mostrarDados();
+       console.log(`cargo: ${this.cargo}`);
+       console.log(`matricula: ${this.matricula}`);
     }
  
-
-
+}
 
 const Funcionario1 = new Funcionario( "medico","020122",["rua brasil"],"joão", "8498898898","joao@hotmail.com",new Date("2024-2-21"),"0210221100")
 
-console.log(Funcionario1)
+
 
 
 
@@ -67,7 +81,7 @@ class Paciente extends Pessoa{
     sintomas: string
     cpf:string 
     
-    constructor ( idade:number,sintomas:string, cpf:string, rg:string, endereco:string[], nome:string, email:string,telefone:string, data_nascimento:Date, ){
+    constructor ( idade:number,sintomas:string, cpf:string, rg:string, endereco:string[], nome:string, email:string,telefone:string, data_nascimento:Date){
      super(endereco, nome,email,telefone, data_nascimento,rg)
 
         this.idade=idade;
@@ -76,13 +90,21 @@ class Paciente extends Pessoa{
         
 
     }
- 
+    mostrarDados(){
+        super.mostrarDados();
+        console.log(`idade ${this.idade}`);
+        console.log(`sitomas: ${this.sintomas}`);
+        console.log(`cpf:${this.cpf}`);
+        
+     }
     
  }
 
 
 
 const paciente1 = new Paciente(34, "febre alta", '89878778', "06547877", ["rua sem nome "], "pedro","pedro@gmail.com","84988885544", new Date("2024-2-21"))
-console.log(paciente1)
 
+paciente1.mostrarDados()
+console.log('================================================')
+Funcionario1.mostrarDados()
 
